8c0ad9041f354ae7f2ff39fbb4e13f2684441165
Pavan Sandhu <pavanbsandhu@gmail.com>
2025-01-11 21:02:19 -0700

    update styling and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,25 @@ function Header() {
   return (
     <div className="header">
       <h1 className="header__logo">active AI</h1>
-      <div className="button__container">
-        <button className="header__button">
-          <p className="header__button-text">JOIN FOR FREE</p>
+      <div className="button__home-container">
+        <button className="button__header">
+          <p className="button__text">Join for Free</p>
         </button>
-        <button className="header__button">
-          <p className="header__button-text">LOGIN</p>
+        <button className="button__header">
+          <p className="button__text">Login</p>
+        </button>
+      </div>
+      <div className="button__loggedin-container">
+        <button className="button__header">
+          <p className="button__text">New Workout</p>
+        </button>
+        <button className="button__header">
+          <p className="button__text">Workout Library</p>
+        </button>
+      </div>
+      <div className="button__profile-container">
+        <button className="button__header">
+          <p className="button__text">Profile</p>
         </button>
       </div>
     </div>
@@ -17,3 +30,30 @@ function Header() {
 }
 
 export default Header;
+
+{
+  /* <div className="header">
+      <h1 className="header__logo">active AI</h1>
+      <div className="button__home-container">
+        <button className="button__join-button">
+          <p className="button__join-text">JOIN FOR FREE</p>
+        </button>
+        <button className="button__login-button">
+          <p className="button__login-text">LOGIN</p>
+        </button>
+      </div>
+      <div className="button__loggedin-container">
+        <button className="header__newworkout-button">
+          <p className="header__newworkout-text">New Workout</p>
+        </button>
+        <button className="header__workoutlib-button">
+          <p className="header__workoutlib-text">Workout Library</p>
+        </button>
+      </div>
+      <div className="button__profile-container">
+        <button className="button__profile-button">
+          <p className="button__profile-text">Profile</p>
+        </button>
+      </div>
+    </div> */
+}
